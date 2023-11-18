a765bfbf0688a0a85a51637bbd593097b9e783e3
Vladimir Usikov <usikovvv@icloud.com>
2023-11-18 14:59:41 +0600

    Add Func

--- program.py
+++ program.py
@@ -23,26 +23,29 @@ Boiled_eggs = [
     'eggs', 'salt'
 ]
 
-
-if set(Cod_liver_salad).issubset(products) is True:
-    print('We can cook cod liver salad')
-else:
-    print("We can't cook cod liver salad")
-
-
-if set(Olivie).issubset(products) is True:
-    print('We can cook olivie')
-else:
-    print("We can't cook olivie")
+def Salads(a, b, c, d, e):
+    if set(a).issubset(b) is True:
+        print('We can cook cod liver salad')
+    else:
+        print("We can't cook cod liver salad")
 
 
-if set(Crab_stick_salad).issubset(products) is True:
-    print('We can cook crab stick salad')
-else:
-    print("We can't cook crab stick salad")
+    if set(c).issubset(b) is True:
+        print('We can cook olivie')
+    else:
+        print("We can't cook olivie")
 
 
-if set(Boiled_eggs).issubset(products) is True:
-    print('We can cook boiled eggs')
-else:
-    print("We can't cook boiled eggs")
+    if set(d).issubset(b) is True:
+        print('We can cook crab stick salad')
+    else:
+        print("We can't cook crab stick salad")
+
+
+    if set(e).issubset(b) is True:
+        print('We can cook boiled eggs')
+    else:
+        print("We can't cook boiled eggs")
+
+
+print(Salads(Cod_liver_salad, products, Olivie, Crab_stick_salad, Boiled_eggs))
