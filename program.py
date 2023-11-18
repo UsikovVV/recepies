bc4775eece252b484fe589d04c079a0e49862e84
Vladimir Usikov <usikovvv@icloud.com>
2023-11-18 15:33:00 +0600

    Add search ingredients

--- program.py
+++ program.py
@@ -13,7 +13,7 @@ Olivie = [
     'mayonnaise', 'salt', 'salted cucumbers', 'eggs'
 ]
 Vegetable_salad = [
-    'tomatoes', 'cucumbers', 'mayonnaise', 'onion'
+    'tomatoes', 'cucumbers', 'mayonnaise', 'onion',
 ]
 Crab_stick_salad = [
     'crab sticks', 'corn', 'rice', 'eggs',
@@ -27,31 +27,51 @@ def Salads(a, b, c, d, e, f):
     if set(a).issubset(b) is True:
         print('We can cook cod liver salad')
     else:
-        print("We can't cook cod liver salad")
+        a_items = []
+        for element in a:
+            if element not in b:
+                a_items.append(element)
+        print('We can not cook cod liver salad. We do not have enough ' + ', '.join(a_items))
 
 
     if set(c).issubset(b) is True:
         print('We can cook olivie')
     else:
-        print("We can't cook olivie")
+        c_items = []
+        for element in c:
+            if element not in b:
+                c_items.append(element)
+        print("We can't cook olivie. We do not have enough " + ', '.join(c_items))
 
 
     if set(d).issubset(b) is True:
         print('We can cook crab stick salad')
     else:
-        print("We can't cook crab stick salad")
+        d_items = []
+        for element in d:
+            if element not in b:
+                d_items.append(element)
+        print("We can't cook crab stick salad. We do not have enough " + ', '.join(d_items))
 
 
     if set(e).issubset(b) is True:
         print('We can cook boiled eggs')
     else:
-        print("We can't cook boiled eggs")
+        e_items = []
+        for element in e:
+            if element not in b:
+                e_items.append(element)
+        print("We can't cook boiled eggs. We do not have enough " + ', '.join(e_items))
 
 
     if set(f).issubset(b) is True:
         print('We can cook vegetable salad')
     else:
-        print("We can't cook vegetable salad")
+        f_items = []
+        for element in f:
+            if element not in b:
+                f_items.append(element)
+        print("We can't cook vegetable salad. We do not have enough " + ', '.join(f_items))
 
 
 print(Salads(Cod_liver_salad, products, Olivie, Crab_stick_salad, Boiled_eggs, Vegetable_salad))
