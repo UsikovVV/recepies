e88ce6411a38ae64c5e64adaab67882e27f6ca45
Vladimir Usikov <usikovvv@icloud.com>
2023-11-18 13:56:30 +0600

    Create list

---
+++ program.py
@@ -0,0 +1,24 @@
+products = [
+    'eggs', 'salt', 'water', 'salted cucumbers',
+    'sausage', 'potato', 'carrot', 'green peas',
+    'corn', 'crab sticks', 'rice', 'tomatoes',
+    'sunflower oil', 'mayonnaise', 'onion', 'cod liver',
+    'cucumbers'
+]
+Liver_salad = [
+    'rice', 'cod liver', 'onion'
+]
+Olivie = [
+    'sausage', 'potato', 'carrot', 'green peas',
+    'mayonnaise', 'salt', 'salted cucumbers', 'eggs'
+]
+Vegetable_salad = [
+    'tomatoes', 'cucumbers', 'mayonnaise', 'onion'
+]
+Crab_stick_salad = [
+    'crab sticks', 'corn', 'rise', 'eggs',
+    'salt', 'mayonnaise'
+]
+Boiled_eggs = [
+    'eggs', 'salt'
+]
